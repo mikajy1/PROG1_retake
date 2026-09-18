bf2c262d08ba4fc87d2c0b05ec344b6458c3486b
Ranaivojaona Mikajy Fehizoro <hei.fehizoro.3@gmail.com>
2026-09-18 14:17:16 +0300

    exo2_3

--- exo2_3.js
+++ exo2_3.js
@@ -1,13 +1,14 @@
-function NombreUn(matrice) {
+function NombreUn(tableau) {
     let count = 0;
-    for (let i = 0; i < matrice.length; i++) {
-        for (let j = 0; j < matrice[i].length; j++) {
-            if (matrice[i][j] === 1) {
+    for (let i = 0; i < tableau.length; i++) {
+
+            if (tableau[i] === 1) {
                 count++;
             }
-        }
+        
     }
     return count;
 
 }
-console.log(NombreUn([[0, 1, 0], [1, 1, 0], [0, 0, 1]]));
+console.log(NombreUn([0, 1, 0, 1, 1, 0, 1]));
+exports.NombreUn = NombreUn;
